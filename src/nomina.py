f12c1336d9b24e186b78809cf1866050a81b6ae6
Melissa Beltran <meli.cesde@gmail.com>
2024-02-06 19:22:18 -0500

    Horas Extra

--- src/nomina.py
+++ src/nomina.py
@@ -1,5 +1,25 @@
 nombre = input("Ingrese el nombre del empleado: ")
-salario = float(input("Ingrese el salario del empleado: "))
+salario = float(input("Ingrese el salario del empleado: $ "))
+
+horastrabajadas = int(input("Ingrese las horas trabajadas: "))
+valorhora = salario / 47
+
+# Calcular las horas extras
+horassemanaleslaborales = 47
+if horastrabajadas > horassemanaleslaborales:
+    horasextras = horastrabajadas - horassemanaleslaborales
+else:
+    horasextras = 0
+
+recargohorasextras = 0.35
+valorhoraextra = valorhora * (1 + recargohorasextras)
+pagobase = horastrabajadas * valorhora
+totalhorasextras = horastrabajadas * valorhoraextra
+salariototal = pagobase + (horasextras * pagobase / horassemanaleslaborales)
+
+print("Las horas extras que trabajó ", nombre, "fueron: ", horasextras)
+print("Valor hora extra : $ ", valorhoraextra)
+print("Total horas extra: $ ", totalhorasextras)
 
 
 
